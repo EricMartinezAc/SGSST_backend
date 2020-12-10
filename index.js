3981f0243ba3073b6b77d72cf8f227fcea34be99
Eric Martinez Acelas <ise.projects@outlook.com>
2020-12-10 05:48:00 -0500

    agregado inicio modulos

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
-const express = requiere('express')
-const app = express()
+try {
+    let database_serve = null;
+    var WebSocketServer = require("ws").Server,
+        http = require("http"),
+        express = require("express"),
+        app = express(),
+        port = process.env.PORT || 8002,
+        cors = require('cors');
+    app.use(cors({
+        origin: true,
+        credentials: true
+    }));
+    app.options('*', cors());
+
+    app.get('/reset', function (req, res) {
+        res.statusCode = 200;
+        res.end('Reset active');
+    });
+    var server = http.createServer(app)
+    server.listen(port);
+
+    console.log("init in port %d", port)
+
+    var wss = new WebSocketServer({ server: server })
+
+    wss.options.maxPayload = 512 * 1024;
+    wss.options.server.timeout = 120000;
+    wss.options.server.keepAliveTimeout = 5000;
+
+
+
+    //conexión websocket
+    try {
+        wss.on("connection", function (ws, req) {
+            ws.on('message', function incoming(data2) {
+                //session:
+                ////epera retornar { "error_": boolean, "resp_": [obj] }
+                try {
+                    var datos = JSON.parse(data2);
+                    console.log("Proceso a ejecutar: ", datos.process_)
+
+                    var ejecucion = require('./process/' + datos.process_ + '');
+                    ejecucion(datos, database_serve).then(function (info) {
+                        ws.send(JSON.stringify({ "error_": false, "resp_": info }));
+                    }).catch(function (err) {
+                        ws.send(JSON.stringify({ "error_": true, "resp_": err }));
+                    });
+
+                } catch (e) {
+                    ws.send(JSON.stringify({ "error_": true, "resp_": e }));//retorna
+                }
+            });
+        });
+
+    } catch (error) {
+        console.log('conexión rota')
+    }
+} catch (error) {
+    console.log('backend OFF')
+}
---
+++ process/consult.js
@@ -0,0 +1,14 @@
+eje = function (datos, db) {
+    return new Promise(function (resolve, reject) {//ver modelo de valores a devolver: modules/models.js
+
+        //datos.data_ : [name, email, pass, passw] = arrays
+        var arrays = datos.data_;
+        var msj_resp = '';
+        var coment = /^[A-Za-z0-9\-\_\.\;\#\$\%\s]{0,100}/;
+        var correo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+
+        console.log(datos, ': ', db)
+    });
+};
+
+module.exports = eje;
