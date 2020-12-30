f0b4be98f7c982a39f6f5ed83a0cbfe2e8612e85
Eric Martinez Acelas <ise.projects@outlook.com>
2020-12-29 21:33:22 -0500

    intentando con fallas realizar consulta SQL

--- process/read.js
+++ process/read.js
@@ -1,11 +1,14 @@
 eje = function (datos, db) {
     return new Promise(function (resolve, reject) {//ver modelo de valores a devolver: modules/models.js
 
-        //datos.data_ : [name, email, pass, passw] = arrays
-        var arrays = datos._data_;
-        var msj_resp = '';
+        //datos.data_ : [name, pass] = arrays
+        let arrays = datos._data_;
+        let usu = arrays[0];
+        let psw = arrays[1];
 
-        console.log(`datos a consultar usuario ${arrays[0]} con psw ${arrays[1]}`)
+        console.log(`datos a consultar usuario ${usu} con psw ${psw}`)
+
+        //Referencia conexion a base de datos
         var connection = db.createConnection({
             host: 'localhost',
             user: 'root',
@@ -13,15 +16,41 @@ eje = function (datos, db) {
             database: 'main_10001'
         });
 
+        //coexion a base de datos
         connection.connect(function (err) {
+            //si ocurre error en conexion
             if (err) {
                 console.error('error connecting: ' + err.stack);
-                reject([arrays, 'reject'])
+                reject([usu, 'no_connected'])
                 return;
+            } else {
+                //si no ocurre error en conexion
+                if (!err) {
+                    console.log('connected as id ' + connection.threadId);
+
+                    //define consulta
+
+                    //ejecutar cosulta
+                    connection.query("SELECT id_persona FROM usuarios WHERE user_name = 'EricMartAc' AND psw = 'Rouse2017*'", function (err, result) {
+                        if (err) {
+                            reject([usu, result])
+                        }
+                        if (!err) {
+                            resolve([usu, 'auth_success'])
+                        } else {
+                            reject([usu, 'undefined_002'])
+                        }
+                    });
+
+
+                } else { // si hay problemas del sistema
+                    reject([usu, 'undefined_001'])
+                }
             }
-            console.log('connected as id ' + connection.threadId);
-            resolve([arrays, 'success'])
+
         });
+
+        connection.end()
 
     });
 };
