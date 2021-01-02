dbc0b21687401b8ce5ce3edc2848f8996dee2283
dev_hp_eric <dev_hp_eric@iseeg.com>
2021-01-01 19:10:55 -0500

    consulta con exito index

--- process/read.js
+++ process/read.js
@@ -3,8 +3,8 @@ eje = function (datos, db) {
 
         //datos.data_ : [name, pass] = arrays
         let arrays = datos._data_;
-        let usu = arrays[0];
-        let psw = arrays[1];
+        let usu = '"' + arrays[0] + '"';
+        let psw = '"' + arrays[1] + '"';
 
         console.log(`datos a consultar usuario ${usu} con psw ${psw}`)
 
@@ -21,31 +21,21 @@ eje = function (datos, db) {
             //si ocurre error en conexion
             if (err) {
                 console.error('error connecting: ' + err.stack);
-                reject([usu, 'no_connected'])
+                reject([usu, 'undefined', 'no_connected'])
                 return;
             } else {
-                //si no ocurre error en conexion
-                if (!err) {
-                    console.log('connected as id ' + connection.threadId);
+                console.log('Conexión exitosa')
+            }
 
-                    //define consulta
+        });
 
-                    //ejecutar cosulta
-                    connection.query("SELECT `id_persona` FROM `usuarios` WHERE `user_name` = 'EricMartAc' AND `psw` = 'Rouse2017*'", function (err, result) {
-                        if (err) {
-                            reject([usu, JSON.stringify(err)])
-                        }
-                        if (!err) {
-                            resolve([usu, 'auth_success'])
-                        } else {
-                            reject([usu, 'undefined_002'])
-                        }
-                    });
-
-
-                } else { // si hay problemas del sistema
-                    reject([usu, 'undefined_001'])
-                }
+        //Realizar consulta
+        connection.query(`SELECT id_persona FROM usuarios WHERE user_name = ${usu} AND psw = ${psw}`, function (error, results) {
+            if (error) {
+                reject([usu, 'datos no encontrados', JSON.stringify(error)])
+            }
+            else {
+                resolve([usu, JSON.parse(JSON.stringify(results))[0]['id_persona'], 'success'])
             }
 
         });
